c5b0f33b4c25f1518b4d6f21cd96658aa45f2d55
MinKhant610 <minkhantiopp@gmail.com>
2024-05-23 14:48:32 +0630

    complet cmake

---
+++ 08 config_file/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "defaultconfig.h"
+int main()
+{
+    std::cout << " Software Version "
+    << ROBOTAPP_VERSION_MAJOR << "."
+    << ROBOTAPP_VERSION_MINOR << std::endl;
+    
+    std::cout << " Product Type " << ProductType << std::endl;
+    std::cout << " Product Year " << ProductYear << std::endl;
+    return 0;
+}
